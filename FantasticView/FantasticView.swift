691360c95b603228b59d702e328dbe90153a3064
Sethmr <sethmr21@gmail.com>
2017-05-22 08:49:00 -0400

    init

---
+++ FantasticView/FantasticView.swift
@@ -0,0 +1,34 @@
+//
+//  FantasticView.swift
+//  FantasticView
+//
+//  Created by Seth Rininger on 5/22/17.
+//  Copyright © 2017 Seth Rininger. All rights reserved.
+//
+
+import UIKit
+
+class FantasticView: UIView {
+
+    
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    var colorCounter = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { timer in
+            UIView.animate(withDuration: 2.0) {
+                self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor
+                self.colorCounter += 1
+            }
+        }
+    
+        scheduledColorChanged.fire()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
